4bcd01ea25f5541aa7f12ed880c2064b48ba4cb8
Gajiu <lea.david@epitech.eu>
2019-08-20 20:14:49 +0200

    :bug: add print for test on macos

--- app/src/components/fileSearch/fileSearch.js
+++ app/src/components/fileSearch/fileSearch.js
@@ -25,6 +25,7 @@ class FileSearch extends React.Component {
      */
     onFilesChange = (files) => {
         this.setState({ files })
+        console.log("jen ai trop marre")
         if (files[0] == undefined) {
             this.setState({
                 message: "Le type de fichier n'est pas supporter.",
@@ -40,6 +41,7 @@ class FileSearch extends React.Component {
      * Affiche un message d'erreur si je le type du fichier n'est pas bon
      */
     onFilesError = (error, file) => {
+        console.log("on sait jamais")
         this.setState({
             message: "error code " + error.code + ": " + error.message,
             open: true,
@@ -67,8 +69,10 @@ class FileSearch extends React.Component {
     async filesUpload(files) {
         const file = files[0];
         const uploadFile = new FileUpload(file, this.props.fileName)
+        console.log("pour commencer")
         await uploadFile.uploadFileToS3().then(async data => {
             if (this.props.redirect == true) {
+                console.log("ici?")
                 await BannerService.saveBannerImgUrl(this.props.banner, data.Location)
                 await this.sleep(2000);
                 this.props.history.push({
@@ -78,6 +82,7 @@ class FileSearch extends React.Component {
                     }
                 })
             } else {
+                console.log("non en fait ci")
                 settingService.createOrUpdateLogo(data.Location)
             }
         })
